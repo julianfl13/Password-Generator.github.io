03f3077601327d25a5cbe2a8482eb51baefe75df
Julian Lopez <julian12lopez@gmail.com>
2020-02-28 16:03:41 -0500

    added java, need to get button working

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+var lowLetter = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers = "1234567890";
 var symbol = "!@#$%^&*()";
 
+function generate (length = 12);
 
+var complete = lowLetter + upperCase + numbers + symbol;
+
+var password ='';
+
+for (var index = 0;index < length; length++){
+        var character = Math.floor(Math.random() * complete.length);
+        password += complete.substring(character,character + 1);
+}
+
+return password;
 
