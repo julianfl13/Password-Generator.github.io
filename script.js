a441dd5ba415f3e19dcbfa0373f7646876f0b200
Julian Lopez <julian12lopez@gmail.com>
2020-03-08 21:01:59 -0400

    committing new code for generator

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-
-
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 var lowLetter = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "1234567890";
 var symbol = "!@#$%^&*()";
 
-function generate (length = 12);
+// Write password to the #password input
+ 
 
-var complete = lowLetter + upperCase + numbers + symbol;
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var password ='';
 
-for (var index = 0;index < length; length++){
-        var character = Math.floor(Math.random() * complete.length);
-        password += complete.substring(character,character + 1);
+  passwordText.value = password;
+
 }
 
-return password;
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+ var passwordLength = prompt("How long would you like the password?");
+  var specialChar = confirm("Would like Special Characters?");
+  var lowerChar = confirm("Would like Lower Case Letters?");
+  var upperChar = confirm("Would like Upper Case Letters?");
+  var numberChar = confirm("Would like Numbers?");
