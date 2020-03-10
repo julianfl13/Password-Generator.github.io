76c96d254b030ef963d029bdce10cd64a194d7f8
Julian Lopez <julian12lopez@gmail.com>
2020-03-10 18:25:21 -0400

    password generator working need to add parameters

--- script.js
+++ script.js
@@ -7,20 +7,70 @@ var symbol = "!@#$%^&*()";
 
 // Write password to the #password input
  
+var passwordLength = 0;
+var passwordString = "";
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 
   function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
 
-  passwordText.value = password;
+
+ passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
- var passwordLength = prompt("How long would you like the password?");
-  var specialChar = confirm("Would like Special Characters?");
-  var lowerChar = confirm("Would like Lower Case Letters?");
-  var upperChar = confirm("Would like Upper Case Letters?");
-  var numberChar = confirm("Would like Numbers?");
+function generatePassword(){
+  
+  var newPass = "";
+
+
+  getPassword();
+  passCriteria();
+
+  for(var i = 0; i < passwordLength; i++){
+    var j = Math.floor(Math.random() * passwordString.length);
+    newPass += passwordString[j];
+  }
+  console.log(newPass);
+  return newPass;
+}
+
+function getPassword(){
+var passLength = prompt("How long would you like the password?");
+passwordLength = parseInt(passLength);
+console.log(passwordLength);
+
+
+}
+
+function passCriteria(){
+
+var lowerChar = confirm("Would like Lower Case Letters?");
+console.log(lowerChar)
+var specialChar = confirm("Would like Special Characters?");
+var upperChar = confirm("Would like Upper Case Letters?");
+var numberChar = confirm("Would like Numbers?");
+
+if (lowerChar){
+  passwordString += lowLetter;
+}
+if(upperChar){
+  passwordString += upperCase;
+}
+if(numberChar){
+  passwordString += numbers;
+}
+if(specialChar){
+  passwordString += symbol;
+}
+console.log(passwordString);
+
+}
+
+ 
